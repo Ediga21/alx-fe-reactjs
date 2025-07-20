43a2d6ff26c9ac493dd451a331abb5b62c3ffcf8
Ediga Idu <iduedigabarnabas@gmail.com>
2025-07-20 18:52:58 +0100

    Refactor UserProfile to use Context API

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,18 @@
-const UserProfile = (props) => {
+// src/components/UserProfile.jsx
+import { useContext } from 'react';
+import UserContext from '../UserContext';
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
