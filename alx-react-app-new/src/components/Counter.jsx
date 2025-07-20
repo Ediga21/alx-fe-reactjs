0237acb8e854843e16854ae1ab22db1b7b42118e
Ediga Idu <iduedigabarnabas@gmail.com>
2025-07-20 17:50:48 +0100

    Update Counter.jsx with Increment, Decrement, Reset buttons

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -1,13 +1,14 @@
-// src/components/Counter.jsx
 import { useState } from 'react';
 
 function Counter() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div style={{ margin: '20px', textAlign: 'center' }}>
       <h2>Counter: {count}</h2>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
