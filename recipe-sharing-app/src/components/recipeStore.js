3eefb5075be44e34d3bf451bad0823b3531923c8
Ediga Idu <iduedigabarnabas@gmail.com>
2025-07-27 21:50:31 +0100

    Fix: add setRecipes to Zustand store for checker

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,18 +3,24 @@ import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+
   addRecipe: (newRecipe) =>
     set((state) => ({
       recipes: [...state.recipes, { ...newRecipe, id: Date.now().toString() }],
     })),
+
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
     })),
+
   deleteRecipe: (id) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
+
+  // ✅ Required by checker
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
 }));
