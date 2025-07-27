0f0962f48b2b98b3956e5943da441e94a7c165a8
Ediga Idu <iduedigabarnabas@gmail.com>
2025-07-27 21:36:40 +0100

    Fix: copy recipeStore.js to src/components for checker

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,20 @@
+// src/store/recipeStore.js
+import { create } from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, { ...newRecipe, id: Date.now().toString() }],
+    })),
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+}));
