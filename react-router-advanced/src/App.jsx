845673d8fd552f3f9002ebbdb6d35c7a06807ed0
Ediga Idu <iduedigabarnabas@gmail.com>
2025-08-24 19:52:33 +0100

    Wrap App with BrowserRouter and finalize routing setup

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
@@ -7,32 +7,34 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/about">About</Link> |{" "}
-        <Link to="/profile">Profile</Link> |{" "}
-        <Link to="/blog/1">Blog Post 1</Link>
-      </nav>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/about">About</Link> |{" "}
+          <Link to="/profile">Profile</Link> |{" "}
+          <Link to="/blog/1">Blog Post 1</Link>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
 
-        {/* Protected route */}
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
+          {/* Protected route */}
+          <Route
+            path="/profile/*"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* Dynamic route */}
-        <Route path="/blog/:id" element={<BlogPost />} />
-      </Routes>
-    </div>
+          {/* Dynamic route */}
+          <Route path="/blog/:id" element={<BlogPost />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
