07c985d5ed0629ba704e2d8817e5bd901d25cc30
Ediga Idu <iduedigabarnabas@gmail.com>
2025-08-24 18:33:59 +0100

    Add advanced routing setup with pages and components

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,39 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import BlogPost from "./pages/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/profile">Profile</Link> |{" "}
+        <Link to="/blog/1">Blog Post 1</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
+        {/* Protected route */}
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Dynamic route */}
+        <Route path="/blog/:id" element={<BlogPost />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
